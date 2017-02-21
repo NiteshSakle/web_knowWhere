7122af1eba0ebade08af346dd21a3a4b4ad8dc22
sunand <sunand@packetzoom.com>
2017-02-21 19:19:03 +0530

    try fixing date format

--- api.py
+++ api.py
@@ -17,7 +17,8 @@ import sys
 import datetime
 import uuid
 import urllib
-import datetime
+from datetime import datetime
+from datetime import date
 
 app = Flask(__name__)
 
@@ -35,16 +36,16 @@ class InfoFilter(logging.Filter):
         return rec.levelno in (logging.DEBUG, logging.INFO)
 
 
-# class SpecializedJSONEncoder(json.JSONEncoder):
-#     def default(o):
-#         if isinstance(o, datetime.date):
-#             return date.strftime("%Y-%m-%d")
-#         elif isinstance(o, datetime.datetime):
-#             return date.strftime("%Y-%m-%d %h:%i:%s")
-#         else:
-#             super(SpecializedJSONEncoder, self).default(o)
-#
-# app.json_encoder = SpecializedJSONEncoder
+class SpecializedJSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, date):
+            return date.strftime("%Y-%m-%d")
+        elif isinstance(o, datetime):
+            return date.strftime("%Y-%m-%d %h:%i:%s")
+        else:
+            super(SpecializedJSONEncoder, self).default(o)
+
+app.json_encoder = SpecializedJSONEncoder
 
 logger = logging.getLogger(__name__)
 
